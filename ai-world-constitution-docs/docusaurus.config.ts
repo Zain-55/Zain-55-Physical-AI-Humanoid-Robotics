304f637dfbaa9209afa43106a4ea0d554e778610
Zain-55 <Zain4u123@gmail.com>
2025-12-10 21:15:54 +0500

    slash-remove

--- ai-world-constitution-docs/docusaurus.config.ts
+++ ai-world-constitution-docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ai-humanoid-book-beryl.vercel.app/',
+  url: 'https://ai-humanoid-book-beryl.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
