cd218e6c8bc13bac3c6255d75c5132600a4ee9d3
Zain-55 <Zain4u123@gmail.com>
2025-12-10 21:12:26 +0500

    update-config

--- ai-world-constitution-docs/docusaurus.config.ts
+++ ai-world-constitution-docs/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://GovernorAiCourse.github.io',
+  url: 'https://ai-humanoid-book-beryl.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-spec/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
